dfda5baf9edcefb0df8371455e104f1b25c22649
HelloWorld017 <deu07115@gmail.com>
2016-02-28 13:53:17 +0900

    Updated wrapper

---
+++ src/bot-wrapper.js
@@ -0,0 +1,46 @@
+'use strict';
+var Library = require('./library');
+
+const SPEED = 1;
+
+class BotWraper{
+	constructor(bot, game){
+		this.bot = bot;
+		this.boundBox = new Library.AABB(new Library.Position(0, 0), new Library.Position(1, 1));
+		this.yaw = 0;
+		this.metadata = {};
+	}
+
+	getBoundBox(){
+		return this.boundBox;
+	}
+
+	rotation(i){
+		if(i === undefined) return this.yaw;
+
+		this.yaw = i;
+		return this;
+	}
+
+	move(){
+		getBoundBox()
+			.nudgeX(parseInt((Math.sin(yaw) * PLAYER_SPEED).toFixed(10)));
+			.nudgeY(parseInt((Math.cos(yaw) * PLAYER_SPEED).toFixed(10)));
+	}
+
+	getBot(){
+		return this.bot;
+	}
+
+	getName(){
+		return this.getBot().name;
+	}
+
+	getSkin(){
+		return this.getBot().skin;
+	}
+
+	getCode(){
+		return this.getBot().code;
+	}
+}
--- src/overrun-game.js
+++ src/overrun-game.js
@@ -5,7 +5,7 @@ var localeval = require('localeval');
 
 const BOARD_SIZE = 5;
 const TURN_COUNT = 40;
-const ROUND_COUNT = 4;
+const ROUND_COUNT = 2;
 const MAX_ACTION_AMOUNT = 4;
 
 const EVAL_TIMEOUT = 50;
@@ -13,17 +13,10 @@ const EVAL_TIMEOUT = 50;
 const START_POS = {
 	0: new Library.AABB(new Library.Position(0, 0), new Library.Position(1, 1)),
 	1: new Library.AABB(new Library.Position(BOARD_SIZE - 1, BOARD_SIZE - 1), new Library.Position(BOARD_SIZE, BOARD_SIZE))
-}
+};
 
 class OverrunGame{
 
-	/**
-	 * constructor - constructor
-	 *
-	 * @param  Bot bot1    description
-	 * @param  Bot bot2    description
-	 * @param  Player[] players description
-	 */
 	constructor(bot1, bot2, players){
 		this.players = players;
 		this.bots = [new BotWrapper(bot1), new BotWrapper(bot2)];
@@ -42,29 +35,34 @@ class OverrunGame{
 		});
 	}
 
-	processRound(){
+	processRound(attackIndex, roundCallback){
+		resetRound();
+
 		bots.forEach((v) => {
 			v.metadata.overallMovement = TURN_COUNT * 2;
 		});
 
 		var turnLog = {};
+		var attack = this.bots[attackIndex];
+		var defence = this.bots.filter((bot, index) => {
+			return index !== attackIndex;
+		})[0];
 
 		async.eachSeries(Library.rangeOf(TURN_COUNT), (i, cb) => {
 			if(turnLog[i] === undefined){
 				turnLog[i] = [];
 			}
 
-			var attack = this.bots[this.attack];
 			attack.metadata.currentMovement = MAX_ACTION_AMOUNT;
 			var attackEvaluator = this.getEvaluator(attack, []);
 
-			localeval(attack.code, attackEvaluator.evaluator, EVAL_TIMEOUT, (err) => {
+			localeval(attack.getCode(), attackEvaluator.evaluator, EVAL_TIMEOUT, (err) => {
 				turnLog[i].push({
 					content: 'turn.change',
 					data: {
 						type: 'attack',
-						name: attack.name,
-						skin: attack.skin,
+						name: attack.getName(),
+						skin: attack.getSkin(),
 						log: attackEvaluator.logs,
 						err: err ? err.toString() : undefined
 					}
@@ -72,6 +70,13 @@ class OverrunGame{
 
 				//deepEqual is okay because this moves on 5*5 grid and size of bots are same
 				if(attack.boundBox.deepEqual(defece.boundBox)){
+					if(defence.defence !== undefined && defence.defence > 0){
+						turnLog[i].push({
+							content: 'turn.defence'
+						});
+						return;
+					}
+
 					turnLog.final = {
 						content: 'turn.attack.win'
 					};
@@ -79,23 +84,20 @@ class OverrunGame{
 					return;
 				}
 
-				var defence = this.bots.filter((bot, index) => {
-					return index !== attack;
-				})[0];
-
 				if(defence.defence !== undefined && defence.defence > 0){
 					defence.defence--;
 				}
 
 				defence.metadata.currentMovement = MAX_ACTION_AMOUNT + 1;
 				var defenceEvaluator = this.getEvaluator(attack, [], true);
-				localeval(defence.code, defenceEvaluator.evaluator, EVAL_TIMEOUT, (err) => {
+
+				localeval(defence.getCode(), defenceEvaluator.evaluator, EVAL_TIMEOUT, (err) => {
 					turnLog[i].push({
 						content: 'turn.change',
 						data: {
 							type: 'defence',
-							name: defence.name,
-							skin: defence.skin,
+							name: defence.getName(),
+							skin: defence.getSkin(),
 							log: defenceEvaluator.logs,
 							err: err ? err.toString() : undefined
 						}
@@ -111,8 +113,14 @@ class OverrunGame{
 				};
 			}
 
-			this.players.forEach((v) => {
-				v.getPlayer().getSocket().emit('simulate', turnLog);
+			roundCallback(turnLog);
+		});
+	}
+
+	start(){
+		async.eachSeries(Library.rangeOf(TURN_COUNT), (k, cb) => {
+			processRound((k % 2), (log) => {
+
 			});
 		});
 	}
@@ -135,7 +143,7 @@ class OverrunGame{
 				if((customLog < 128 && typeof data === 'string') && data.length < 256){
 					customLog++;
 					logObject.push({
-						content: 'turn.text',
+						content: content,
 						data: data
 					});
 				}
@@ -149,6 +157,30 @@ class OverrunGame{
 		};
 
 		var evalObject = {
+			log: function(content){
+				if(typeof content !== 'string'){
+					log('turn.err', new Error('turn.content.not.string'), true);
+					return false;
+				}
+
+				log('turn.text', content, true);
+				return true;
+			},
+
+			move: function(){
+				var err = check(1);
+				if(err){
+					log('turn.err', err, true);
+					return false;
+				}
+
+				bot.metadata.currentMovement--;
+				bot.metadata.overallMovement--;
+				bot.move();
+				log('turn.move');
+				return true;
+			},
+
 			rotate: function(amount){
 				var err = check(1);
 				if(Number.isInteger(amount)){
@@ -157,34 +189,14 @@ class OverrunGame{
 
 				if(err){
 					log('turn.err', err, true);
-					return;
+					return false;
 				}
 
 				bot.metadata.currentMovement--;
 				bot.metadata.overallMovement--;
 				bot.yaw += amount;
 				log('turn.rotate', amount);
-			},
-
-			log: function(content){
-				if(typeof content !== 'string'){
-					log('turn.err', new Error('turn.content.not.string'), true);
-				}
-
-				log(turn, content);
-			},
-
-			move: function(){
-				var err = check(1);
-				if(err){
-					log('turn.err', err, true);
-					return;
-				}
-
-				bot.metadata.currentMovement--;
-				bot.metadata.overallMovement--;
-				bot.move();
-				log(i, 'turn.move');
+				return true;
 			},
 
 			status: function(){
@@ -192,7 +204,7 @@ class OverrunGame{
 					'left-overall': bot.metadata.overallMovement,
 					'left-current': bot.metadata.currentMovement,
 					'turn': turn
-				}
+				};
 			},
 
 			getBots: function(){
@@ -218,6 +230,7 @@ class OverrunGame{
 				bot.metadata.defence = 2;
 				bot.metadata.currentMovement -= 6;
 				bot.metadata.overallMovement -= 6;
+				log('turn.defence');
 			};
 		}
 
