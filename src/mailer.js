363cd228d26033c54f32e6602c67b5f38126ec77
Khinenw <khinenw@khinenw.tk>
2016-03-23 14:41:36 +0000

    Updated mail

--- src/mailer.js
+++ src/mailer.js
@@ -1,5 +1,21 @@
+var ejs = require('ejs');
+var fs = require('fs');
+
+var transporter = require('nodemailer').createTransport(`smtps://${global.config['gmail-id']}%40gmail.com:${global.config['gmail-pw']}@smtp.gmail.com`);
+
+var bodyTemplate = {
+	from: `"${global.config['email-from']}" <${global.config['email-from-address']}>`
+};
+
 module.exports = {
 	send: (subject, sendTo, template, content) => {
-
+		var mail = bodyTemplate;
+		mail.subject = subject;
+		mail.to = sendTo;
+		fs.readFile('/views/' + template + '.ejs', (err, res) => {
+			mail.html = ejs.render(res, content);
+			transporter.sendMail(mail);
+		});
 	}
-}
+};
+
