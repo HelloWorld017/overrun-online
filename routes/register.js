9e9955b437514378abe2c2657306fd72432fd6bc
HelloWorld017 <deu07115@gmail.com>
2016-03-24 12:18:21 +0900

    Oops

--- routes/register.js
+++ routes/register.js
@@ -104,7 +104,7 @@ router.all('/', (req, resp, next) => {
 										authToken: authToken
 									});
 
-									mailer.send(global.translation['subject-verify-email'], 'verify-email', email, {
+									mailer.send(global.translation['subject-verify-email'], email, 'verify-email', {
 										authToken: authToken
 									});
 
