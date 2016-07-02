424c8c181f0e26518b3441bf329fb36afafdd3ab
HelloWorld017 <deu07115@gmail.com>
2016-07-02 19:38:51 +0900

    Updated API

--- plugins/meiro/api-list.js
+++ plugins/meiro/api-list.js
@@ -1,6 +1,6 @@
 module.exports = {
 	title: global.translator('plugin.meiro.api.title'),
-	content: ['log', 'turnLeft', 'turnRight', 'move', 'carveWall', 'x', 'y', 'direction', 'checkWall', 'save', 'load', 'items', 'moveResult'].map((v) => {
+	content: ['rule', 'log', 'turnLeft', 'turnRight', 'move', 'carveWall', 'x', 'y', 'direction', 'checkWall', 'save', 'load', 'items', 'objects', 'moveResult'].map((v) => {
 		return {
 			title: global.translator(`plugin.meiro.api.${v}.title`),
 			content: global.translator(`plugin.meiro.api.${v}.content`)
